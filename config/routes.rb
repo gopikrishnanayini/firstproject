a92e273e40d1c6c5bd59b3971b520202d46c7e3f
gopikrishna <nayinigopi93@gmail.com>
2017-03-27 17:24:11 +0530

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'articles/new'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+    get 'articles/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
